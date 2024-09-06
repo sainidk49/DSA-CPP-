87e2a899ac05ab57c8aedb305652c5d50f9c9e63
Deepak Saini <dpksaini49@gmail.com>
2024-09-06 17:28:06 +0530

    create first file

---
+++ 01_dataType.cpp
@@ -0,0 +1,92 @@
+#include <iostream> // preprocessor directive
+using namespace std;
+
+int main()
+{
+    // ================ print and divid line =================
+    // cout << "dk saini 1 " << endl;              // 1. for the end line
+    // cout << "dk saini 2 " << "\n";              // 2.  for the end line
+    // cout << "dk saini 3 \n";                    // 3. for the end line
+    // cout << "dk saini 4 \ndk saini 5 \n";      // 4. for the end line
+    // cout << "dk saini 6 ";
+    // return 0;
+
+    // ================ data type and size =================
+    // int age = 25;
+    // char grade = 'A';
+    // float PI = 3.14f;
+    // bool isCorrect = true;
+    // double price = 100.99;
+
+    // cout << age << endl;
+    // cout << grade << endl;
+    // cout << PI << endl;
+    // cout << isCorrect << endl;      // return => 0 and 1
+    // cout << price << endl;          // return => 100
+    // return 0;
+
+    // ============ Implicit conversion ====================
+    // char grade = 'A';
+    // int convert = grade;
+
+    // cout << grade << endl;
+    // cout << convert << endl;
+
+    // return 0;
+
+    // ============ Explicit conversion ====================
+    // double price = 100.99;
+    // int convert = (int)price;
+
+    // cout << price << endl;
+    // cout << convert << endl;
+
+    // return 0;
+
+    // // ============ take input by user ====================
+    // int age;
+    // cout << "enter your age : ";
+    // cin >> age;
+    // cout << "your age is " << age << endl;
+
+    // return 0;
+
+    // ============ Operations ====================
+
+    //// arthemetic Operator
+    // int a = 9, b = 5;
+    // cout << "Sum of a and b : " << (a + b) << endl;
+    // cout << "Sub of a and b : " << (a - b) << endl;
+    // cout << "Mul of a and b : " << (a * b) << endl;
+    // cout << "div of a and b : " << (a / (double)b) << endl;
+    // cout << "mod of a and b : " << (a % b) << endl;
+
+    //// Relational (Comparison) Operator
+    // int x = 10, y = 20;
+    // cout << (x == y) << endl; // false (0)
+    // cout << (x != y) << endl; // true (1)
+    // cout << (x < y) << endl;  // true (1)
+    // cout << (x > y) << endl;  // false (0)
+
+    //// Logical Operator
+    // bool a = true, b = false;
+    // cout << (a && b) << endl; // false (0)
+    // cout << (a || b) << endl; // true (1)
+    // cout << (!a) << endl;     // false (0)
+
+    //// Ternary Operator
+    // int a = 10, b = 20;
+    // int max = (a > b) ? a : b;
+    // cout << "Max: " << max << endl;
+
+    //// Bitwise Operators
+    int a = 5, b = 9;
+    cout << (a & b) << endl;  // AND
+    cout << (a | b) << endl;  // OR
+    cout << (a ^ b) << endl;  // XOR
+    cout << (~a) << endl;     // NOT
+    cout << (a << 1) << endl; // Left shift
+    cout << (a >> 1) << endl; // Right shift
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-hello
+========== Memory Sizes ======================
+
+=> 1TB = 1024GB
+=> 1GB = 1024MB
+=> 1MB = 1024KB
+=> 1KB = 1000bytes
+=> 1byte = 8bit
+
+
+========== DataTypes and bytes ===============
+
+=> char = 1 byte
+=> bool = 1 byte
+=> int = 4 bytes
+=> float = 4 byte
+=> double = 8 byte
